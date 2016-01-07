62fbb89dae00479fb3c414cd728b4f95c6285d8d
Arvin Singla <arvin.singla@acquia.com>
2016-01-07 11:21:36 -0500

    Added async request handling to blocking operation.

--- switcher.js
+++ switcher.js
@@ -3,10 +3,15 @@
 var application_root = __dirname,
     executable = "codesend",
     express = require("express"),
-  //  bodyParser = require('body-parser'),
+    async = require("async"),
     exec = require('child_process').exec;
+    //  bodyParser = require('body-parser'),
 
 var app = express();
+
+var queue = async.queue(function(req, callback) {
+  exec('./' + executable + ' ' + req.params.id, callback);
+}, 1);
 
 //app.use(bodyParser());
 
@@ -21,13 +26,15 @@ app.get('/api/switch', function (req, res) {
 // RF Switch API Endpoint.
 app.get('/api/switch/:id', function (req, res){
 
-  return exec('./' + executable + ' ' + req.params.id,
-    function (error, stdout, stderr) {
+  console.log("Request: " + req.originalUrl);
+
+  queue.push(req, function (error, stdout, stderr) {
       if (error !== null) {
         return res.status(500, {error: 'something blew up'}).send('FAILED');
       }
       return res.status(200).send('SUCCESS');
   });
+
 });
 
 // Launch server.
